e58b88996c0bd0a916ec0b85f6e571022d250ec0
AsistenciaApp <asistenciaapp@ejemplo.com>
2026-02-18 01:18:44 -0400

    Preparar estructura docs/ para GitHub Pages (PWA)

---
+++ docs/static/js/sw.js
@@ -0,0 +1,41 @@
+// Service Worker para PWA
+const CACHE_NAME = 'voleibol-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/styles.css',
+  '/static/js/app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
